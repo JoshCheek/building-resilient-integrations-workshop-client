e8b70425fa93b708fd2863dd1805f407802a756e
Josh Cheek <josh.cheek@gmail.com>
2016-09-23 22:04:47 -0700

    Refactor: push registration into the instance

--- lib/resilint.rb
+++ lib/resilint.rb
@@ -1,30 +1,20 @@
 require 'rest-client'
 require 'json'
 
+
 class Resilint
   def self.registered(opts)
-    opts[:user_id] ||= begin
-      user_id = register(opts.fetch(:base_url), opts.fetch(:user_name))
-      opts.fetch(:post_registration, Proc.new {}).call(user_id)
-      user_id
-    end
     new(opts)
   end
 
-  def self.register(base_url, user_name)
-    json = RestClient.post "#{base_url}/v1/register?userName=#{user_name}", {}
-    # => "{\"user\":\"e707b38c-1d53-4be2-a4c7-dd3fc9fc77b8\",\"name\":\"JoshCheek\"}"
-    body = JSON.parse(json)
-    body.fetch 'user'
-  end
-
-  attr_accessor :base_url, :user_id, :user_name, :timeout
+  attr_accessor :base_url, :user_id, :user_name, :timeout, :post_registration
 
   def initialize(opts)
-    self.base_url  = opts.fetch :base_url
-    self.user_id   = opts.fetch :user_id
-    self.user_name = opts.fetch :user_name
-    self.timeout   = opts.fetch :timeout
+    self.base_url          = opts.fetch :base_url
+    self.user_name         = opts.fetch :user_name
+    self.timeout           = opts.fetch :timeout
+    self.post_registration = opts.fetch :post_registration, Proc.new {}
+    self.user_id           = opts[:user_id] || register
   end
 
   def excavate
@@ -36,10 +26,22 @@ class Resilint
     # => {"bucketId"=>"499728b5-c311-4c59-ac3d-132686dfa036", "gold"=>{"units"=>4}}
     body = JSON.parse(json)
     body.fetch 'bucketId'
+  rescue RestClient::Exceptions::ReadTimeout
   end
 
   def store(bucket_id)
     body = RestClient.post "#{base_url}/v1/store?userId=#{user_id}&bucketId=#{bucket_id}", {}
-    body == 'true'
+    body == 'true' or raise(body)
+  end
+
+  private
+
+  def register
+    json = RestClient.post "#{base_url}/v1/register?userName=#{user_name}", {}
+    # => "{\"user\":\"e707b38c-1d53-4be2-a4c7-dd3fc9fc77b8\",\"name\":\"JoshCheek\"}"
+    body = JSON.parse(json)
+    user_id = body.fetch 'user'
+    post_registration.call(user_id)
+    user_id
   end
 end
